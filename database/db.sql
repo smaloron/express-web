fa80cbe47506c2cb1c2d1eb181bfd702afa3773e
Sebastien Maloron <seb@moi.com>
2022-04-11 10:04:16 +0200

    schema SQL

---
+++ database/db.sql
@@ -0,0 +1,10 @@
+USE formation_express;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT UNSIGNED AUTO_INCREMENT,
+    user_name VARCHAR(30) NOT NULL,
+    user_firstName VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    hashed_password VARCHAR(128) NOT NULL,
+    PRIMARY KEY (id)
+);
